a9a074772c845734159a89530438aaf714c7ba7d
Tereza-Khachatryan <saterya97@gmail.com>
2024-09-04 12:18:44 +0400

    create countPayment file for 2 test

---
+++ countPayment.js
@@ -0,0 +1,68 @@
+//task 2.3
+
+
+const payments = [
+    {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe", // Name of the student
+    payedAmount: 1600, // Amount payed with this transaction
+    year: 2020, // The payment year
+    },
+    {
+    studentId: 1, // Unique for each student
+    studentName: "John Doe",
+    payedAmount: 1500,
+    year: 2021
+    },
+    {
+    studentId: 1, // Unique for each student
+    studentName: "John Doe",
+    payedAmount: 1400,
+    year: 2022
+    },
+    {
+    studentId: 1, // Unique for each student
+    studentName: "John Doe",
+    payedAmount: 1300,
+    year: 2023
+    },
+    {
+    studentId: 2, // Unique for each student
+    studentName: "Lu Kang",
+    payedAmount: 1500,
+    
+    year: 2020
+    },
+    {
+    studentId: 2, // Unique for each student
+    studentName: "Lu Kang",
+    payedAmount: 1400,
+    year: 2021
+    },
+    {
+    studentId: 2, // Unique for each student
+    studentName: "Lu Kang",
+    payedAmount: 1300,
+    year: 2023
+    },
+    {
+    studentId: 2, // Unique for each student
+    studentName: "Lu Kang",
+    payedAmount: 1200,
+    year: 2023
+    }
+    ];
+
+
+    
+function countPayment(arr) {
+    let count = 0
+    for (const value of arr) {
+      if (value.payedAmount > 1500) {
+        count++
+      }
+    }
+    return count
+  }
+  
+  console.log(countPayment(payments))
