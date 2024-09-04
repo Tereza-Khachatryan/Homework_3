ceda9e09c59d0c828323b713b69af0c85ea5be89
Tereza-Khachatryan <saterya97@gmail.com>
2024-09-04 12:13:17 +0400

    create moreThan60 file for 1 test

---
+++ MoreThan60.js
@@ -0,0 +1,81 @@
+//task 1.3
+
+const evaluations = [
+    {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    score: 70
+    },
+    {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 60
+    },
+    {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 50
+    },
+    {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    
+    score: 60
+    },
+    {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 50
+    },
+    {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 40
+    },
+    {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    score: 45
+    },
+    {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 35
+    },
+    {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 3, // Unique identifier for each course
+    
+    courseName: "JS Level 1",
+    score: 25
+    }
+    ];
+
+   function getMoreThan60(arr){
+    let result = []
+    for (let value of arr){
+        if(value.score > 60){
+            result.push(value)
+        }
+    }
+    return result
+   }
+
+   console.log(getMoreThan60(evaluations))
