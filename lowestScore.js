6a38e8baca341a760cba3307eadf40d0b5ea9345
Tereza-Khachatryan <saterya97@gmail.com>
2024-09-04 12:21:51 +0400

    create lowest score file for 1 test

---
+++ lowestScore.js
@@ -0,0 +1,79 @@
+//task 1.1
+
+const evaluations = [
+    {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    score: 70
+    },
+    {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 60
+    },
+    {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 50
+    },
+    {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    
+    score: 60
+    },
+    {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 50
+    },
+    {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 40
+    },
+    {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    score: 45
+    },
+    {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 35
+    },
+    {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 3, // Unique identifier for each course
+    
+    courseName: "JS Level 1",
+    score: 25
+    }
+    ];
+
+    function get (arr) {
+        let min = Infinity
+        for (let item of arr){
+            if (item.score < min) min = item.score
+        }
+        return min
+    }
+
+    console.log (get(evaluations))
